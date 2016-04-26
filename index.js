456e50688662f3c9e981bc60492ff1a0ebb5efc5
Ramsey Chapin <ramsey@ramseychapin.com>
2016-04-26 07:21:19 +0000

    Fourth change to index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var file = fs.readFileSync('/index.html', 'utf8').toString();
+
+var file = fs.readFileSync('index.html', 'utf8')
 
 app.get('/', function(request, response) {
   response.send(file)
